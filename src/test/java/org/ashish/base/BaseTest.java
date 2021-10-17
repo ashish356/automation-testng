c9574fcab13dbac33ec77f11197865ebb3a0b85c
ashish <ashish2008kumar@gmial.com>
2021-10-17 08:59:12 +0100

    added ui test

--- src/test/java/org/ashish/base/BaseTest.java
+++ src/test/java/org/ashish/base/BaseTest.java
@@ -4,6 +4,7 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.ashish.constants.Constants;
 import org.ashish.tests.api.UserServiceTest;
 import org.ashish.utils.ExtentManager;
 import org.openqa.selenium.WebDriver;
@@ -14,32 +15,48 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+
 
 public class BaseTest {
 
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceTest.class);
 
-    public static final ExtentReports extentReports= ExtentManager.getInstance();
+    public static final ExtentReports extentReports = ExtentManager.getInstance();
     public static ExtentTest extentTest;
     public WebDriver driver;
 
-    @AfterMethod
-    public void tearDown() {
 
-        if (driver != null) {
-            driver.quit();
-        }
-
-        if (extentReports != null) {
-            try {
-                extentReports.endTest(extentTest);
-                extentReports.flush();
-            } catch (Exception e) {
-                extentTest.log(LogStatus.FAIL, e);
-                LOGGER.error(String.format("Exception is : %s", e));
-            }
-        }
-
+    @BeforeMethod
+    public void setUp() {
+        createDirectoryIfDoesNotExist("reports");
+        System.out.println("I am inside Before Moethod of Bast Test");
     }
-}
+
+    private void createDirectoryIfDoesNotExist(final String directoryName) {
+        final File directory = new File(Constants.PROJECT_DIRECTORY + "/" + directoryName);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+    }
+
+        @AfterMethod
+        public void tearDown () {
+
+            if (driver != null) {
+                driver.quit();
+            }
+
+            if (extentReports != null) {
+                try {
+                    extentReports.endTest(extentTest);
+                    extentReports.flush();
+                } catch (Exception e) {
+                    extentTest.log(LogStatus.FAIL, e);
+                    LOGGER.error(String.format("Exception is : %s", e));
+                }
+            }
+
+        }
+    }
